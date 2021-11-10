a53dcdfe90ddbbdce078e04cc911fb051fe4427f
Ansfrid Pereira <89774958+ansfridpereira@users.noreply.github.com>
2021-11-10 21:49:07 +0530

    Add files via upload
    Facad In Java

---
+++ FacadeinJava/src/refactoring_guru/facade/example/some_complex_media_library/Videofile.java
@@ -0,0 +1,95 @@
+package refactoring_guru.facade.example.some_complex_media_library;
+
+import refactoring_guru.facade.example.some_complex_media_library.*;
+
+import java.io.File;
+
+
+ class VideoFile {
+    private String name;
+    private String codecType;
+
+    public VideoFile(String name) {
+        this.name = name;
+        this.codecType = name.substring(name.indexOf(".") + 1);
+    }
+
+    public String getCodecType() {
+        return codecType;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+ interface Codec {
+}
+
+ class MPEG4CompressionCodec implements Codec {
+    public String type = "mp4";
+
+}
+
+ class OggCompressionCodec implements Codec {
+    public String type = "ogg";
+}
+
+ class CodecFactory {
+    public static Codec extract(VideoFile file) {
+        String type = file.getCodecType();
+        if (type.equals("mp4")) {
+            System.out.println("CodecFactory: extracting mpeg audio...");
+            return new MPEG4CompressionCodec();
+        }
+        else {
+            System.out.println("CodecFactory: extracting ogg audio...");
+            return new OggCompressionCodec();
+        }
+    }
+}
+
+ class BitrateReader {
+    public static VideoFile read(VideoFile file, Codec codec) {
+        System.out.println("BitrateReader: reading file...");
+        return file;
+    }
+
+    public static VideoFile convert(VideoFile buffer, Codec codec) {
+        System.out.println("BitrateReader: writing file...");
+        return buffer;
+    }
+}
+
+ class AudioMixer {
+    public File fix(VideoFile result){
+        System.out.println("AudioMixer: fixing audio...");
+        return new File("tmp");
+    }
+}
+
+ class VideoConversionFacade {
+    public File convertVideo(String fileName, String format) {
+        System.out.println("VideoConversionFacade: conversion started.");
+        VideoFile file = new VideoFile(fileName);
+        Codec sourceCodec = CodecFactory.extract(file);
+        Codec destinationCodec;
+        if (format.equals("mp4")) {
+            destinationCodec = new OggCompressionCodec();
+        } else {
+            destinationCodec = new MPEG4CompressionCodec();
+        }
+        VideoFile buffer = BitrateReader.read(file, sourceCodec);
+        VideoFile intermediateResult = BitrateReader.convert(buffer, destinationCodec);
+        File result = (new AudioMixer()).fix(intermediateResult);
+        System.out.println("VideoConversionFacade: conversion completed.");
+        return result;
+    }
+}
+
+ class Demo {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+        // ...
+    }
+}
