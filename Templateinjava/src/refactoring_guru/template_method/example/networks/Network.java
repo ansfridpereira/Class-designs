bddb7afaf62986839f67c31a47860858fa1ba7d8
Ansfrid Pereira <89774958+ansfridpereira@users.noreply.github.com>
2021-11-10 08:18:58 +0530

    Add files via upload
    Tempelate in Java

---
+++ Templateinjava/src/refactoring_guru/template_method/example/networks/Network.java
@@ -0,0 +1,154 @@
+package refactoring_guru.template_method.example.networks;
+
+import refactoring_guru.template_method.example.networks.Facebook;
+import refactoring_guru.template_method.example.networks.Network;
+import refactoring_guru.template_method.example.networks.Twitter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public abstract class Network {
+    String userName;
+    String password;
+
+    Network() {}
+
+    public boolean post(String message) {
+        // Authenticate before posting. Every network uses a different
+        // authentication method.
+        if (logIn(this.userName, this.password)) {
+            // Send the post data.
+            boolean result =  sendData(message.getBytes());
+            logOut();
+            return result;
+        }
+        return false;
+    }
+
+    abstract boolean logIn(String userName, String password);
+    abstract boolean sendData(byte[] data);
+    abstract void logOut();
+}
+
+class Facebook extends Network {
+    public Facebook(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public boolean logIn(String userName, String password) {
+        System.out.println("\nChecking user's parameters");
+        System.out.println("Name: " + this.userName);
+        System.out.print("Password: ");
+        for (int i = 0; i < this.password.length(); i++) {
+            System.out.print("*");
+        }
+        simulateNetworkLatency();
+        System.out.println("\n\nLogIn success on Facebook");
+        return true;
+    }
+
+    public boolean sendData(byte[] data) {
+        boolean messagePosted = true;
+        if (messagePosted) {
+            System.out.println("Message: '" + new String(data) + "' was posted on Facebook");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void logOut() {
+        System.out.println("User: '" + userName + "' was logged out from Facebook");
+    }
+
+    private void simulateNetworkLatency() {
+        try {
+            int i = 0;
+            System.out.println();
+            while (i < 10) {
+                System.out.print(".");
+                Thread.sleep(500);
+                i++;
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+class Twitter extends Network {
+
+    public Twitter(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public boolean logIn(String userName, String password) {
+        System.out.println("\nChecking user's parameters");
+        System.out.println("Name: " + this.userName);
+        System.out.print("Password: ");
+        for (int i = 0; i < this.password.length(); i++) {
+            System.out.print("*");
+        }
+        simulateNetworkLatency();
+        System.out.println("\n\nLogIn success on Twitter");
+        return true;
+    }
+
+    public boolean sendData(byte[] data) {
+        boolean messagePosted = true;
+        if (messagePosted) {
+            System.out.println("Message: '" + new String(data) + "' was posted on Twitter");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void logOut() {
+        System.out.println("User: '" + userName + "' was logged out from Twitter");
+    }
+
+    private void simulateNetworkLatency() {
+        try {
+            int i = 0;
+            System.out.println();
+            while (i < 10) {
+                System.out.print(".");
+                Thread.sleep(500);
+                i++;
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+ class Demo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Network network = null;
+        System.out.print("Input user name: ");
+        String userName = reader.readLine();
+        System.out.print("Input password: ");
+        String password = reader.readLine();
+
+        // Enter the message.
+        System.out.print("Input message: ");
+        String message = reader.readLine();
+
+        System.out.println("\nChoose social network for posting message.\n" +
+                "1 - Facebook\n" +
+                "2 - Twitter");
+        int choice = Integer.parseInt(reader.readLine());
+
+        // Create proper network object and send the message.
+        if (choice == 1) {
+            network = new Facebook(userName, password);
+        } else if (choice == 2) {
+            network = new Twitter(userName, password);
+        }
+        network.post(message);
+    }
+}
