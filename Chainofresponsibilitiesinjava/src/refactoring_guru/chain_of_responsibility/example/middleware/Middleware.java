b591335512fc26abb18a0d8ae144fbafcf3787ad
Ansfrid Pereira <89774958+ansfridpereira@users.noreply.github.com>
2021-11-11 21:41:45 +0530

    Add files via upload
    Chain of Responsibilities

---
+++ Chainofresponsibilitiesinjava/src/refactoring_guru/chain_of_responsibility/example/middleware/Middleware.java
@@ -0,0 +1,132 @@
+package refactoring_guru.chain_of_responsibility.example.middleware;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public abstract class Middleware {
+    private Middleware next;
+    public Middleware linkWith(Middleware next) {
+        this.next = next;
+        return next;
+    }
+    public abstract boolean check(String email, String password);
+
+    protected boolean checkNext(String email, String password) {
+        if (next == null) {
+            return true;
+        }
+        return next.check(email, password);
+    }
+}
+class ThrottlingMiddleware extends Middleware {
+    private int requestPerMinute;
+    private int request;
+    private long currentTime;
+
+    public ThrottlingMiddleware(int requestPerMinute) {
+        this.requestPerMinute = requestPerMinute;
+        this.currentTime = System.currentTimeMillis();
+    }
+    public boolean check(String email, String password) {
+        if (System.currentTimeMillis() > currentTime + 60_000) {
+            request = 0;
+            currentTime = System.currentTimeMillis();
+        }
+
+        request++;
+        if (request > requestPerMinute) {
+            System.out.println("Request limit exceeded!");
+            Thread.currentThread().stop();
+        }
+        return checkNext(email, password);
+    }
+}class UserExistsMiddleware extends Middleware {
+    private Server server;
+
+    public UserExistsMiddleware(Server server) {
+        this.server = server;
+    }
+
+    public boolean check(String email, String password) {
+        if (!server.hasEmail(email)) {
+            System.out.println("This email is not registered!");
+            return false;
+        }
+        if (!server.isValidPassword(email, password)) {
+            System.out.println("Wrong password!");
+            return false;
+        }
+        return checkNext(email, password);
+    }
+}class RoleCheckMiddleware extends Middleware {
+    public boolean check(String email, String password) {
+        if (email.equals("admin@example.com")) {
+            System.out.println("Hello, admin!");
+            return true;
+        }
+        System.out.println("Hello, user!");
+        return checkNext(email, password);
+    }
+}class Server {
+    private Map<String, String> users = new HashMap<>();
+    private Middleware middleware;
+
+    public void setMiddleware(Middleware middleware) {
+        this.middleware = middleware;
+    }
+    public boolean logIn(String email, String password) {
+        if (middleware.check(email, password)) {
+            System.out.println("Authorization have been successful!");
+
+            // Do something useful here for authorized users.
+
+            return true;
+        }
+        return false;
+    }
+
+    public void register(String email, String password) {
+        users.put(email, password);
+    }
+
+    public boolean hasEmail(String email) {
+        return users.containsKey(email);
+    }
+
+    public boolean isValidPassword(String email, String password) {
+        return users.get(email).equals(password);
+    }
+}
+class Demo {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static Server server;
+
+    private static void init() {
+        server = new Server();
+        server.register("admin@example.com", "admin_pass");
+        server.register("user@example.com", "user_pass");
+
+        // All checks are linked. Client can build various chains using the same
+        // components.
+        Middleware middleware = new ThrottlingMiddleware(2);
+        middleware.linkWith(new UserExistsMiddleware(server))
+                .linkWith(new RoleCheckMiddleware());
+
+        // Server gets a chain from client code.
+        server.setMiddleware(middleware);
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        boolean success;
+        do {
+            System.out.print("Enter email: ");
+            String email = reader.readLine();
+            System.out.print("Input password: ");
+            String password = reader.readLine();
+            success = server.logIn(email, password);
+        } while (!success);
+    }
+}
